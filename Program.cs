1c03b4c127ad35769cbcd27be34a5f6aaa9a8ad4
Lucas Lopes <Lucas Lopes@DESKTOP-MVL07S5>
2024-05-30 07:45:36 -0300

    primeira versão Desafio Smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("11999999999", "Tijolão", "123456789", 2);
+nokia.Ligar();
+nokia.InstalarAplicativo("Chrome");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("iPhone");
+Smartphone iphone = new Iphone("11999999999", "X pro", "123456789", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
